40b034910709c2899b4a6dc027f7d250eb4613ab
jac <joao.coutinho@sebasi.pt>
2023-05-17 11:25:38 +0100

    Edit profile
    modal to edit user profile informations(cover image, profile image, name, username, bio)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Toaster } from 'react-hot-toast';
 import { SessionProvider } from 'next-auth/react';
+import EditModal from '@/components/modals/EditModal'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
     {/* <Modal actionLabel='Submit' isOpen title='Test Modal'/> */}
     <RegisterModal />
     <LoginModal />
+    <EditModal />
       <Layout> 
         <Component {...pageProps} />
       </Layout>
